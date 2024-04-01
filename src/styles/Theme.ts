e2d94045b07e58baa9d2b99a26e8df2cd28c71e5
Arefevpro <arefevmsk@gmail.com>
2024-04-01 20:48:08 +0300

    Add files via upload

---
+++ src/styles/Theme.ts
@@ -0,0 +1,9 @@
+export const theme = {
+  colors: {
+    accentGreen: "#00f32d", // Зелый
+    accentWhite: "#FFFFFF", // Белый
+    colorGray: "#ABB2BF",   // Серый 
+
+    bgColorDark: "#282C33", // цвет фона поумолчанию
+  },
+};
